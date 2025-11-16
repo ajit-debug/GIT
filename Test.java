81e63fa724cf3422721b7780963ea8c7e3d43718
Ajit Kumar <Ajit.Kumar@Trivium-eSolutions.com>
2025-11-16 20:21:47 +0530

    commit message 4

--- Test.java
+++ Test.java
@@ -5,4 +5,6 @@ public class Test {
         int sum = num1 + num2;
         
         System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+        System.out.println("Print line2");
+
 }
